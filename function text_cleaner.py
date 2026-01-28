70197963b6e07ba6ffa47060d74aad3bb264c298
davydkaulko <davydkaulko@gmail.com>
2026-01-28 08:40:51 -0500

    Create function text_cleaner.py
    clean and count text

---
+++ function text_cleaner.py
@@ -0,0 +1,12 @@
+#phyton function that make text clear lower and count № of characters
+text = input("Type your text: ")
+def clean_text(text):
+    cleaned = text.strip()
+    cleaned = cleaned.lower()
+    count = len(cleaned)
+    return cleaned, count
+
+result_text, length = clean_text(text)
+
+print("Cleaned text:", result_text)
+print("Length:", length)
